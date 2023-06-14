066a7e6648e1f0ff7327b4c3e0bd913f8d71cd8b
SLiYDD <mudritsyn@yandex.ru>
2023-06-14 18:42:55 +0300

    add FilmStorage , UserStorage, FilmService, UserService

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,56 +1,23 @@
 package ru.yandex.practicum.filmorate.service;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
+public interface FilmService {
+    Film saveFilm(Film film);
 
-@Service
-public class FilmService {
-    private HashMap<Integer, Film> films;
-    private int count;
+    Film updateFilm(Film film);
 
+    Film findFilmById(int id);
 
-    public FilmService() {
-        this.films = new HashMap<>();
-        count = 1;
-    }
+    List<Film> getAllFilm();
 
+    void deleteFilm(int id);
 
-    public Film saveFilm(Film film) {
-        if (films.containsValue(film)) {
-            for (Film value : films.values()) {
-                if (value.equals(film)) return value;
-            }
-        }
-        film.setId(count);
-        films.put(count, film);
-        count++;
-        return film;
-    }
+    void addLike(int filmId, int userId);
 
+    void deleteLike(int filmId, int userId);
 
-    public Film changeFilm(Film film) {
-        Optional<Film> filmOpt = Optional.ofNullable(films.get(film.getId()));
-        if (filmOpt.isEmpty()) {
-            throw new NotFoundException("This Film does not exist");
-        }
-        Film filmDb = filmOpt.get();
-        BeanUtils.copyProperties(film, filmDb, "id");
-        return films.put(film.getId(), filmDb);
-    }
-
-
-    public List<Film> getAllFilm() {
-        if (films.isEmpty()) {
-            throw new NotFoundException("The list of films is empty");
-        }
-        return new ArrayList<>(films.values());
-    }
+    List<Film> findPopular(int count);
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/InMemoryUserStorage.java
@@ -0,0 +1,59 @@
+package ru.yandex.practicum.filmorate.storage.impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.*;
+
+@Component
+public class InMemoryUserStorage implements UserStorage {
+    private HashMap<Integer, User> users = new HashMap<>();
+    private static Integer globalId = 1;
+
+    @Override
+    public Optional<User> addUser(User user) {
+        if (users.containsValue(user)) {
+            return Optional.empty();
+        }
+        var userBuild = user.toBuilder()
+                .friends(new HashSet<>())
+                .id(generateId())
+                .build();
+        users.put(userBuild.getId(), userBuild);
+        return Optional.of(users.get(userBuild.getId()));
+    }
+
+    @Override
+    public Optional<User> updateUser(User user) {
+        var userDb = users.get(user.getId());
+        if (userDb != null) {
+            BeanUtils.copyProperties(user , userDb, "id", "friends");
+            return Optional.of(userDb);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        if (users.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public Optional<User> findUserById(Integer id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    @Override
+    public boolean deleteUser(int id) {
+        return Optional.ofNullable(users.remove(id)).isPresent();
+    }
+
+    private int generateId() {
+        return globalId++;
+    }
+}
